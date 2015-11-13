8bda322355a75d65b920ee2324891ab676a65f19
renyu310 <zhang.yuren@hotmail.com>
2015-11-13 22:31:42 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #成功情况
+      #闪现页面
+      flash[:success] = "欢迎来到示例App!"
+      #重定向页面 redirect_to user——url(@user)
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
+
 end
